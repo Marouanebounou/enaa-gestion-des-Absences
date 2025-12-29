050260480eebb8a823e42255c6ce0265e0db733d
soufiane <soufianeelbasraoui0@gmail.com>
2025-12-29 10:47:10 +0100

    Update dashboard and styles

--- tableubord.js
+++ tableubord.js
@@ -2,7 +2,7 @@
 //let students= [{"nom":"amine","prenom":"Bounou","retardTime":"5:50","absences":5,"retards":3,"presence":20,"group":"G1"},{"nom":"Ali","prenom":"Bounou","retardTime":"7:20","absences":4,"retards":6,"presence":18,"group":"G2"},{"nom":"Mohamed","prenom":"Bounou","retardTime":"2:45","absences":8,"retards":2,"presence":15,"group":"G3"},{"nom":"Youssef","prenom":"Bounou","retardTime":"8:50","absences":2,"retards":7,"presence":21,"group":"G1"},{"nom":"soufiane","prenom":"Bounou","retardTime":"4:00","absences":2,"retards":3,"presence":11,"group":"G2"}];
 
 const afficherStatus =()=>{
-        
+
         const data=localStorage.getItem('students');
         const students =JSON.parse(data);
          console.log(students);
